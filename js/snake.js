767dbf6d3bd179ac43a7d8ac985b2fc341557197
Felipe Castro <fcastro@opsolve.com>
2018-08-18 20:59:19 -0300

    Added pause functionality and new icons

--- js/snake.js
+++ js/snake.js
@@ -10,7 +10,7 @@ const gameConfig = {
     allowedInputs: [37, 38, 39, 40]
 };
 
-var canvas, context, scoreBoard, direction, prevDirection, snake, game, food, score, inputQueue, fullGame;
+var canvas, context, scoreBoard, direction, prevDirection, snake, game, food, score, inputQueue, fullGame, gamePaused;
 reset();
 
 function reset() {
@@ -21,6 +21,7 @@ function reset() {
     if (window.innerWidth > 1000) {
         canvas.width = 225;
         document.getElementById('controls').setAttribute("style", "display: none");
+        document.getElementById('game-pause').setAttribute("style", "display: none");
     } else {
         canvas.width = window.innerWidth - 15;
     }
@@ -43,92 +44,94 @@ function reset() {
         score: 0,
         deadMove: {}
     };
+    gamePaused = false;
 
     do {
         food = generateFood(0, gameConfig.height - 1);
     } while (isOnSnake(food.x, food.y))
 
     game = window.setInterval(function () {
-        fullGame.snakeMoves.push({
-            snake: JSON.parse(JSON.stringify(snake)),
-            food: JSON.parse(JSON.stringify(food)),
-            score: score
-        });
-        direction = inputQueue.shift() || direction;
-        switch (direction) {
-            case 37:
-                if (prevDirection == 39) {
-                    snake.unshift({ x: snake[0].x + 1, y: snake[0].y });
-                    direction = 39;
-                } else {
-                    snake.unshift({ x: snake[0].x - 1, y: snake[0].y });
-                }
-                break;
-            case 38:
-                if (prevDirection == 40) {
-                    snake.unshift({ x: snake[0].x, y: snake[0].y + 1 });
-                    direction = 40;
-                } else {
-                    snake.unshift({ x: snake[0].x, y: snake[0].y - 1 });
-                }
-                break;
-            case 39:
-                if (prevDirection == 37) {
-                    snake.unshift({ x: snake[0].x - 1, y: snake[0].y });
-                    direction = 37;
-                } else {
-                    snake.unshift({ x: snake[0].x + 1, y: snake[0].y });
-                }
-                break;
-            case 40:
-                if (prevDirection == 38) {
-                    snake.unshift({ x: snake[0].x, y: snake[0].y - 1 });
-                    direction = 38;
-                } else {
-                    snake.unshift({ x: snake[0].x, y: snake[0].y + 1 });
-                }
-                break;
-            default:
-                break;
-        }
+        if (!gamePaused) {
+            fullGame.snakeMoves.push({
+                snake: JSON.parse(JSON.stringify(snake)),
+                food: JSON.parse(JSON.stringify(food)),
+                score: score
+            });
+            direction = inputQueue.shift() || direction;
+            switch (direction) {
+                case 37:
+                    if (prevDirection == 39) {
+                        snake.unshift({ x: snake[0].x + 1, y: snake[0].y });
+                        direction = 39;
+                    } else {
+                        snake.unshift({ x: snake[0].x - 1, y: snake[0].y });
+                    }
+                    break;
+                case 38:
+                    if (prevDirection == 40) {
+                        snake.unshift({ x: snake[0].x, y: snake[0].y + 1 });
+                        direction = 40;
+                    } else {
+                        snake.unshift({ x: snake[0].x, y: snake[0].y - 1 });
+                    }
+                    break;
+                case 39:
+                    if (prevDirection == 37) {
+                        snake.unshift({ x: snake[0].x - 1, y: snake[0].y });
+                        direction = 37;
+                    } else {
+                        snake.unshift({ x: snake[0].x + 1, y: snake[0].y });
+                    }
+                    break;
+                case 40:
+                    if (prevDirection == 38) {
+                        snake.unshift({ x: snake[0].x, y: snake[0].y - 1 });
+                        direction = 38;
+                    } else {
+                        snake.unshift({ x: snake[0].x, y: snake[0].y + 1 });
+                    }
+                    break;
+                default:
+                    break;
+            }
 
 
-        prevDirection = direction;
+            prevDirection = direction;
 
-        keyLock = false;
+            keyLock = false;
 
-        if (snake[0].x < 0 || snake[0].x >= gameConfig.width || snake[0].y < 0 || snake[0].y >= gameConfig.height) {
-            endGame();
-            return;
-        }
-
-        for (var i = 1; i < snake.length; i++) {
-            if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            if (snake[0].x < 0 || snake[0].x >= gameConfig.width || snake[0].y < 0 || snake[0].y >= gameConfig.height) {
                 endGame();
                 return;
             }
-        }
 
-        if (snake[0].x == food.x && snake[0].y == food.y) {
-            do {
-                food = generateFood(0, gameConfig.height - 1);
-            } while (isOnSnake(food.x, food.y))
-            score++;
-            scoreBoard.textContent = score;
-        } else {
-            snake.pop();
-        }
-
-        for (var i = 0; i < gameConfig.height; i++) {
-            for (var j = 0; j < gameConfig.width; j++) {
-                drawCell(i, j, 'rgb(255, 255, 255)');
+            for (var i = 1; i < snake.length; i++) {
+                if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+                    endGame();
+                    return;
+                }
             }
-        }
-        for (var i = 0; i < snake.length; i++) {
-            drawCell(snake[i].x, snake[i].y, 'rgb(0, 0, 0)');
-        }
-        drawCell(food.x, food.y, 'rgb(255, 0, 0)');
 
+            if (snake[0].x == food.x && snake[0].y == food.y) {
+                do {
+                    food = generateFood(0, gameConfig.height - 1);
+                } while (isOnSnake(food.x, food.y))
+                score++;
+                scoreBoard.textContent = score;
+            } else {
+                snake.pop();
+            }
+
+            for (var i = 0; i < gameConfig.height; i++) {
+                for (var j = 0; j < gameConfig.width; j++) {
+                    drawCell(i, j, 'rgb(255, 255, 255)');
+                }
+            }
+            for (var i = 0; i < snake.length; i++) {
+                drawCell(snake[i].x, snake[i].y, 'rgb(0, 0, 0)');
+            }
+            drawCell(food.x, food.y, 'rgb(255, 0, 0)');
+        }
     }, document.getElementById('game-speed').value);
 }
 
@@ -166,6 +169,9 @@ function endGame() {
 }
 
 function addInputToQueue(event) {
+    if (event == 80) {
+        gamePaused = !gamePaused;
+    }
     if (event == 32) {
         clearInterval(game);
         context.clearRect(0, 0, canvas.width, canvas.height);
